070e26ed75e6b9225aa29201f5e7e8e1da83d62e
shivanikewat9691 <shivani.s@pixoatic.com>
2023-12-12 18:12:33 +0530

    signup page deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get 'courses/hadoop'
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root 'repos#index'
+  root 'repos#index'
   resources :repos
   # resources :courses
   get 'home', to: 'repos#home',as: 'home'
