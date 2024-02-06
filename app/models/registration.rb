58593b6790b411294204d267768654def4926739
shivanikewat9691 <shivani.s@pixoatic.com>
2024-02-06 16:05:54 +0530

    show message

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,3 +1,9 @@
 class Registration < ApplicationRecord
-	self.table_name = :registrations
+  self.table_name = :registrations
+  validates :your_name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
+  validates :course_of_interest, presence: true
+  validates :message, presence: true
+
 end
