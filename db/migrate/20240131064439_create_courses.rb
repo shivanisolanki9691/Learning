ca0c83e2278830fa9b1465dd19d67475d21298d4
shivanikewat9691 <shivani.s@pixoatic.com>
2024-03-12 18:12:01 +0530

    solve error

--- db/migrate/20240131064439_create_courses.rb
+++ db/migrate/20240131064439_create_courses.rb
@@ -1,21 +1,21 @@
 class CreateCourses < ActiveRecord::Migration[6.1]
   def change
     unless table_exists?(:courses)
-    create_table :courses do |t|
-      t.string :course_name
-      t.text :description
-      t.string :price
-      t.string :discounted_price
-      t.string :live_classes
-      t.string :quizzes
-      t.string :projects
-      t.string :doubt_support
-      t.string :interview_questions_covered
-      t.string :lifetime_access
-      t.string :image_url
+      create_table :courses do |t|
+        t.string :course_name
+        t.text :description
+        t.string :price
+        t.string :discounted_price
+        t.string :live_classes
+        t.string :quizzes
+        t.string :projects
+        t.string :doubt_support
+        t.string :interview_questions_covered
+        t.string :lifetime_access
+        t.string :image_url
 
-      t.timestamps
+        t.timestamps
+      end
     end
-  end
   end
 end
